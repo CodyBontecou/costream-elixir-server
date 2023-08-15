b08a72be9fdefc2420ac84250fd6d6a0da30dcbf
Cody Bontecou <bontecouc@gmail.com>
2023-08-14 17:04:18 -1000

    commented out for now

--- http_server/lib/http_server_web/controllers/live_controller.ex
+++ http_server/lib/http_server_web/controllers/live_controller.ex
@@ -10,13 +10,14 @@ defmodule HttpServerWeb.LiveController do
     json(
       conn,
       JSONParser.fetch_and_parse_data(Constants.getUrls().getLive)
-      |> LeagueInfo.format_info()
+      # |> LeagueInfo.format_info()
     )
   end
 
   defp get_live_by_league_slug(league_slug) do
     JSONParser.fetch_and_parse_data(Constants.getUrls().getLive)
     |> Enum.filter(fn event -> event.league["slug"] == league_slug end)
-    |> LeagueInfo.format_info()
+
+    # |> LeagueInfo.format_info()
   end
 end
